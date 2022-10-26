7a93cc3ff6583dc66fb31f1744c1e4e675ef8194
Alex Turaev <altex_76@mail.ru>
2022-10-27 00:26:08 +0300

    first commit

---
+++ src/main/java/hw2_14/com/example/hw2_14demo/Hw214demoApplication.java
@@ -0,0 +1,13 @@
+package hw2_14.com.example.hw2_14demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw214demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw214demoApplication.class, args);
+	}
+
+}
